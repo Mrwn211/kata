ef2d2e780e9749cd74023f2c31d0d175f331e769
Mrwn211 <merwanguerrib@gmail.com>
2019-10-07 15:20:59 +0200

    Create VowelCount.js

---
+++ 7kyu/VowelCount.js
@@ -0,0 +1,10 @@
+function getCount(str) {
+  var vowelsCount = 0;
+  
+  for (let i = 0 ; i < str.length ; i ++){
+    if (str[i].match(/[aeiou]/)) {
+    vowelsCount++;
+    }
+  } 
+  return vowelsCount;
+}
