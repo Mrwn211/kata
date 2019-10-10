9cb235ea2354b546097aa979a424983dfed1f053
Mrwn211 <merwanguerrib@gmail.com>
2019-10-10 12:16:52 +0200

    Update FormTheLargest.js

--- 7kyu/FormTheLargest.js
+++ 7kyu/FormTheLargest.js
@@ -2,5 +2,6 @@
 // Given a number , Return _The Maximum number _ could be formed from the digits of the number given.
 
 function maxNumber(n){
-  return parseInt(n.toString().split("").sort().reverse().join(""));
+  let number = parseInt(n.toString().split("").sort().reverse().join(""));
+  return number;
 }
